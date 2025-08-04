b7c2db0ef40cda641ac39c2c75c3d64a2878e878
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-08-04 15:07:55 -0300

    feat: implement partial logic in cart.js and item.js and test in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-const cart = [];
+import * as cartService from './services/cart.js';
+import createItem from "./services/item.js";
+
+const myCart = [];
+const myWishList = [];
+
+
+const item1 = await createItem("mouse", 20.99, 1);
+const item2 = await createItem("teclado mecânico", 39.99, 3);
+
+console.log("Welcome to your Shopee Cart!")
+
+// adicionei dois itens ao carrinho
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+//remover item
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+
+//deletar item
+await cartService.deleteItem(myCart, item1.name);
+
+// deletei dois itens do carrinho
+// await cartService.deleteItem(myCart, item2.name);
+// await cartService.deleteItem(myCart, item1.name);
+
+// exibi os itens do carrinho
+await cartService.displayCart(myCart);
+
+// calculei total de itens do carrinho
+await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,24 +1,63 @@
-// Quais ações meu carrinho pode fazer?
-
-
 // CASOS DE USO:
 
-// adicionar item no carrinho
-async function addItem(userCart){
-
+// ✅ adicionar item no carrinho
+async function addItem(userCart, item) {
+    userCart.push(item);
 }
 
-// deletar o item do carrinho
-async function deleteItem(userCart, name){
+// ✅calcular o total 
+async function calculateTotal(userCart) {
+    console.log("\nShopee Cart TOTAL IS:");
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+    console.log(`🛒 Total: ${result}`);
+}
 
+// ✅deletar o item do carrinho
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    }
 }
 
 // remover um item - diminui um item
-async function removeItem(userCart, index){
+async function removeItem(userCart, item) {
 
+    // 1. encontrar o índice do item
+    const indexFound = userCart.findIndex((p) => p.name === item.name)
+
+    // 2. caso não encontre o item
+    if (indexFound == -1){
+        console.log("item não encontrado");
+        return;
+    }
+
+    // 3. item > 1, subtrair um item
+    if (userCart[indexFound].quantity > 1){
+        userCart[indexFound].quantity -= 1;
+        return;
+    }
+
+    // 4. Item = 1, deletar o item
+    if (userCart[indexFound.quantity == 1]){
+        userCart.splice(indexFound, 1);
+        return;
+    }
 }
 
-// calcular o total 
-async function calculateTotal(userCart){
-    
+// ✅ mostrar o que tem dentro do carrinho
+async function displayCart(userCart) {
+    console.log("\nShopee cart list: ");
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal = ${item.subtotal()}`);
+    });
 }
+
+export {
+    addItem,
+    calculateTotal,
+    deleteItem,
+    removeItem,
+    displayCart
+};
